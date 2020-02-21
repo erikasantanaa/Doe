8517cc09d2d03309881dc3ade26cee1b3130a636
erika santana <erika.araujo12344@gmail.com>
2020-02-20 22:09:47 -0300

    segundo commit

--- server.js
+++ server.js
@@ -1,8 +1,29 @@
+//configurar o servidor
 const express = require("express")
 const server = express() 
 
-server.get("/", function(req, res) { //pega a barra, e executa uma serie de funcionalidades.
-    return res.send("ok. Chegue aqui!")
+//configurar o servidor para apresentar arquivos extras/ css
+server.use(express.static('maratonaDev'))
+
+
+//configurando a template engine (chPamar o index.html)
+const nunjucks = require("nunjucks")
+nunjucks.configure("./maratonaDev", {
+    express: server
 })
 
-server.listen(3000) //servidor.
+const donors = [
+    {
+
+    }
+    ]
+
+//Configurar a apresentação da página / render: renderizar, contruir alguma coisa.
+server.get("/", function(req, res) { //pega a barra, e executa uma serie de funcionalidades.
+    return res.render("index.html")
+})
+
+//Ligar o servidor e permitir o acesso na porta 3000
+server.listen(3000, function() {
+    console.log("Iniciei o servidor.")
+}) 
